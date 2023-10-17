25ab9e3eff0faff55a8ef728a6b73a9685dcd68d
AlejaEJ <alejaespejo96@gmail.com>
2023-10-17 14:37:54 -0500

    Task 03/Form created with lodash.throttle

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const saveToLocalStorage = () => {
+    const formData = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+form.addEventListener('input', throttle(saveToLocalStorage, 500));
+
+window.addEventListener('load', () => {
+    const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (savedData) {
+        emailInput.value = savedData.email;
+        messageInput.value = savedData.message;
+    }
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = '';
+    messageInput.value = '';
+});
