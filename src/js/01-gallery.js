f5575a5fe37c9b9107f87111e605e8ca3769bd7a
AlejaEJ <alejaespejo96@gmail.com>
2023-10-17 12:41:20 -0500

    Task 01/import SimplelightBox/copy the code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+const createItem = (item) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`;
+};
+
+const itemsContainer = document.querySelector('.gallery');
+for (const item of galleryItems) {
+    const newItem = createItem(item);
+    itemsContainer.innerHTML += newItem; 
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
